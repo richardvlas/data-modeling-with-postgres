1211ce98e2e7df58adaaf9ed25dd51d512b27281
Richard Vlas <vlasrichard@gmail.com>
2022-02-12 13:42:34 +0100

    added query lists

--- sql_queries.py
+++ sql_queries.py
@@ -5,6 +5,7 @@ song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
+
 # CREATE TABLES
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
@@ -63,4 +64,15 @@ CREATE TABLE IF NOT EXISTS time (
 """)
 
 
-# INSERT RECORDS
+# QUERY LISTS
+create_table_queries = [songplay_table_create, 
+                        user_table_create,
+                        song_table_create, 
+                        artist_table_create, 
+                        time_table_create]
+
+drop_table_queries = [songplay_table_drop, 
+                      user_table_drop, 
+                      song_table_drop,
+                      artist_table_drop, 
+                      time_table_drop]
